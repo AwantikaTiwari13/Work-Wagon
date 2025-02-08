16397d7f01b844b91be977b42653c60d0006b4dc
awantika <tiwariawantika13@gmail.com>
2025-02-08 15:22:18 +0530

    React setup and Navbar Completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Navbar from "./components/shared/Navbar.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between  mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job <span className="text-[#F83002]">Portal</span>{" "}
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex  font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="flex  gap-4 space-y-2">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                </Avatar>
+                <div>
+                  <h4 className="font-medium">Awantika</h4>
+                  <p className="text-small text-muted-foreground">
+                    Lorem ipsum dolor sit amet.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 my-2 text-gray-600">
+                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                  {/* <User2 /> */}
+                  <Button variant="link">View Profile</Button>
+                </div>
+                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                  {/* <LogOut /> */}
+                  <Button variant="link">Logout</Button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
